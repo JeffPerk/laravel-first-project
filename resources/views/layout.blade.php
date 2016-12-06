249eddf0b3ae8f4910ec1db328771a67b1e9ec12
JeffPerk <jeff.perk87@gmail.com>
2016-12-06 12:16:16 -0700

    Messed with elixir

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <title>Laravel</title>
 
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
   </head>
   <body>
     @yield('content')
